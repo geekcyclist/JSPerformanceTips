9477d15d55c325e95f739411e0bbf85dbc912993
Gary Ray <gray.ray@gmail.com>
2016-09-15 16:23:46 -0600

    Add demo for filtering and sorting

---
+++ 3-Filters/after/scripts/app.js
@@ -0,0 +1,64 @@
+var employeeList = [];  //create an empty list to store employee objects
+fillEmployeeList();
+
+document.getElementById('output').innerHTML = "There are "  + employeeList.length + " employees listed." ;
+
+/**
+ *  Iterate over the company object stored in companyDetails.js
+ *  to fill the employeeList array for future use.
+ **/
+function fillEmployeeList(){
+    var divList, division, divName, empList, employee, empName;
+    divList = company[0].divisions;
+    for(var i = 0, dCount = divList.length; i < dCount; i++) {
+        division = divList[i];
+        divName = division.divisionName;
+        empList = division.employees;
+        //We can filter and sort the emplist for each division
+        // before adding to the employeeList.
+        //Don't forget you can chain operations'
+        var filteredEmployees = empList.filter(function(item){
+            return item.isActive;    
+        }).sort(function(a, b){
+            return a.name.last === b.name.last ? 0 : +(a.name.last > b.name.last) || -1;    
+        });
+        // Make sure you remember to replace empList with filteredEmployees...
+        for(var j = 0, eCount = filteredEmployees.length; j < eCount; j++) {
+            employee = filteredEmployees[j];
+            var emp = new Employee(
+                employee.name.first,
+                employee.name.last,
+                employee.email,
+                divName
+            )
+            employeeList.push(emp);
+        }                                                       
+    }
+    //we could apply our filter and sorting here but it's a little more complicated
+    // since we have already flattened the list - have to sort division and name...
+}
+
+
+/**
+ * Employee object constructor
+ **/
+function Employee (firstName, lastName, email, division){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.division = division;
+    this.fullName = firstName + ' ' + lastName;
+    switch (division){
+        case 'Administration':
+            this.location = '10th Floor';
+            break;
+        case 'HR':
+            this.location = '6th Floor';
+            break;
+        case 'Development':
+            this.location = 'Satellite Campus';
+            break;
+        default:
+            alert(this.fullName + ' should move to the parking garage with ' +
+                               'thier red Swingline(TM) stapler.' );
+    }
+}
---
+++ 3-Filters/before/scripts/app.js
@@ -0,0 +1,60 @@
+var employeeList = [];  //create an empty list to store employee objects
+fillEmployeeList();
+
+document.getElementById('output').innerHTML = "There are "  + employeeList.length + " employees listed." ;
+
+/**
+ *  Iterate over the company object stored in companyDetails.js
+ *  to fill the employeeList array for future use.
+ **/
+function fillEmployeeList(){
+    var divList, division, divName, empList, employee, empName;
+    divList = company[0].divisions;
+    for(var i = 0, dCount = divList.length; i < dCount; i++) {
+        division = divList[i];
+        divName = division.divisionName;
+        empList = division.employees;
+        for(var j = 0, eCount = empList.length; j < eCount; j++) {
+            employee = empList[j];
+            // Restrict to only active employees
+            // but don't be a NOOB and use === true
+            if(employee.isActive){
+                var emp = new Employee(
+                    employee.name.first,
+                    employee.name.last,
+                    employee.email,
+                    divName
+                )
+                employeeList.push(emp);
+            }
+            // Now that we have only active employees,
+            // do we really want to loop again and write
+            // our own sort?
+        }                                                       
+    }
+}
+
+
+/**
+ * Employee object constructor
+ **/
+function Employee (firstName, lastName, email, division){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.division = division;
+    this.fullName = firstName + ' ' + lastName;
+    switch (division){
+        case 'Administration':
+            this.location = '10th Floor';
+            break;
+        case 'HR':
+            this.location = '6th Floor';
+            break;
+        case 'Development':
+            this.location = 'Satellite Campus';
+            break;
+        default:
+            alert(this.fullName + ' should move to the parking garage with ' +
+                               'thier red Swingline(TM) stapler.' );
+    }
+}
